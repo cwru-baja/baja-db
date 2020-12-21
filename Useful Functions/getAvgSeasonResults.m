47a17779cc790f52ff73d8e5cc7df24a26f964e3
Kevin Salipante <ksalipante@gmail.com>
2020-12-21 10:39:55 -0500

    Further Notebook Updates

--- Useful Functions/getAvgSeasonResults.m
+++ Useful Functions/getAvgSeasonResults.m
@@ -3,9 +3,9 @@ yearResults = cell(3, 3);
 
 for i = 1:size(dataIn, 1)
     if contains(dataIn{i, 3}, year)
-        yearResults(1, :) = dataIn(i, :)
-        yearResults(2, :) = dataIn(i+1, :)
-        yearResults(3, :) = dataIn(i+2, :)
+        yearResults(1, :) = dataIn(i, :);
+        yearResults(2, :) = dataIn(i+1, :);
+        yearResults(3, :) = dataIn(i+2, :);
         
         break;
     end
@@ -18,15 +18,15 @@ uniNumbers = sort(str2double(unique(uniNames(2, :))));
 seasRanks = cell(1, 2);
 
 for i = 1:size(uniNumbers, 2)
-    indicies = str2double(uniNames(2,:)) == uniNumbers(i)
+    indicies = str2double(uniNames(2,:)) == uniNumbers(i);
     
-    names = uniNames(1, indicies)
+    names = uniNames(1, indicies);
     
     compCounter = 0;
     pointsTotal = 0;
     
     for j = 1:size(yearResults, 1)
-        kkb = intersect(names, yearResults{j, 1}.universityName)
+        kkb = intersect(names, yearResults{j, 1}.universityName);
         
         if kkb ~= ""
             if size(kkb, 1) == 1
@@ -40,17 +40,17 @@ for i = 1:size(uniNumbers, 2)
     
     
     newRow = cell(1, 2);
-    newRow(1, 1) = {convertStringsToChars(names(1))}
-    newRow(1, 2) = {num2str(pointsTotal(1)/compCounter)}
+    newRow(1, 1) = {convertStringsToChars(names(1))};
+    newRow(1, 2) = {num2str(pointsTotal(1)/compCounter)};
     
-    seasRanks = [seasRanks; newRow]
+    seasRanks = [seasRanks; newRow];
 end
 
 seasRanks = seasRanks(2:size(seasRanks, 1), :);
 
-sortingColumn = str2double(seasRanks(:,2))
+sortingColumn = str2double(seasRanks(:,2));
 
 [sortedValues, sortOrder] = sort(sortingColumn, 'descend');
 
-seasRanks = seasRanks(sortOrder, :)
+seasRanks = seasRanks(sortOrder, :);
 end
--- Useful Functions/getIronTeamResults.m
+++ Useful Functions/getIronTeamResults.m
@@ -3,9 +3,9 @@ yearResults = cell(3, 3);
 
 for i = 1:size(dataIn, 1)
     if contains(dataIn{i, 3}, year)
-        yearResults(1, :) = dataIn(i, :)
-        yearResults(2, :) = dataIn(i+1, :)
-        yearResults(3, :) = dataIn(i+2, :)
+        yearResults(1, :) = dataIn(i, :);
+        yearResults(2, :) = dataIn(i+1, :);
+        yearResults(3, :) = dataIn(i+2, :);
         
         break;
     end
@@ -18,15 +18,15 @@ uniNumbers = sort(str2double(unique(uniNames(2, :))));
 schmidtRanks = cell(1, 2);
 
 for i = 1:size(uniNumbers, 2)
-    indicies = str2double(uniNames(2,:)) == uniNumbers(i)
+    indicies = str2double(uniNames(2,:)) == uniNumbers(i);
     
-    names = uniNames(1, indicies)
+    names = uniNames(1, indicies);
     
     allThree = true;
     pointsTotal = 0;
     
     for j = 1:size(yearResults, 1)
-        kkb = intersect(names, yearResults{j, 1}.universityName)
+        kkb = intersect(names, yearResults{j, 1}.universityName);
         
         if kkb ~= ""
             if size(kkb, 1) == 1
@@ -40,21 +40,20 @@ for i = 1:size(uniNumbers, 2)
     end
     
     if allThree == true
-        fprintf("sup b")
         
         newRow = cell(1, 2);
-        newRow(1, 1) = {convertStringsToChars(names(1))}
-        newRow(1, 2) = {num2str(pointsTotal(1))}
+        newRow(1, 1) = {convertStringsToChars(names(1))};
+        newRow(1, 2) = {num2str(pointsTotal(1))};
         
-        schmidtRanks = [schmidtRanks; newRow]
+        schmidtRanks = [schmidtRanks; newRow];
     end
 end
 
 schmidtRanks = schmidtRanks(2:size(schmidtRanks, 1), :);
 
-sortingColumn = str2double(schmidtRanks(:,2))
+sortingColumn = str2double(schmidtRanks(:,2));
 
 [sortedValues, sortOrder] = sort(sortingColumn, 'descend');
 
-schmidtRanks = schmidtRanks(sortOrder, :)
+schmidtRanks = schmidtRanks(sortOrder, :);
 end
