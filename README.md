f94850e246490fe2870042b444d91927050e1c53
Kevin Salipante <ksalipante@gmail.com>
2020-12-21 19:29:44 -0500

    2D plotting cleanup

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 This repository is a MATLAB-based database for analyzing Baja SAE results. It contains results for all competitions from 2007 onward. A suite of functions can be used to analyze the results from these competitions. The notebook file `bajaStatsNotebook.mlx` should be used to interact with any functions.
 
-## Functions
+## License
+
+MIT
--- Useful Functions/graphResults2D.m
+++ Useful Functions/graphResults2D.m
@@ -12,26 +12,5 @@ function compGraph = graphResults2D(dataIn, compNumber, xDataName, yDataName)
     
     figure(fig);
     
-    % storing points in array for output
-    compGraph = [xData, yData];
-    
-    % marking data points with at least one value of 0
-    outliers = ~sum(compGraph' == 0)';
-    
-    % erasing these 0 points because they mess up regression
-    compGraphClean = compGraph(outliers(:), :);
-    
-    % splitting up clean data
-    xDataClean = compGraphClean(:, 1);
-    yDataClean = compGraphClean(:, 2);
-    
-    % generating and plotting linear regression to clean data
-    fit_line = fitlm(xDataClean, yDataClean)
-    plot(fit_line);
-    
-    % naming
-    xlabel(xDataName);
-    ylabel(yDataName);
-    
     title(xDataName + " vs " + yDataName + " at " + dataIn{(cell2mat(dataIn(:, 2)) == compNumber), 3});
 end
