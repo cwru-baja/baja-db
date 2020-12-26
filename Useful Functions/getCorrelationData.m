1bddbdf974f26139558482776358ff1a6deaa33c
Kevin Salipante <ksalipante@gmail.com>
2020-12-26 17:27:12 -0500

    Correlation Function Progress

--- Useful Functions/getCorrelationData.m
+++ Useful Functions/getCorrelationData.m
@@ -1,4 +1,10 @@
 function rSqrs = getCorrelationData(event, dataIn)
+
+names = {'compName', 'rSqu', 'rSquAdj', 'points'};
+types = {'string', 'int8', 'int8', 'double'};
+
+corrs = table('Size', [1, 5], 'VariableNames', names, 'VariableTypes', types);
+
 rSqrs = zeros([size(dataIn, 1) 1]);
 for i = 22:size(dataIn, 1) + 21
     try
