2160a674069cd7ebbcea37e9a7a1104628433c17
Kevin Salipante <ksalipante@gmail.com>
2020-12-26 17:38:13 -0500

    More Correlation Progress

--- Useful Functions/getCorrelationData.m
+++ Useful Functions/getCorrelationData.m
@@ -3,18 +3,17 @@ function rSqrs = getCorrelationData(event, dataIn)
 names = {'compName', 'rSqu', 'rSquAdj', 'points'};
 types = {'string', 'int8', 'int8', 'double'};
 
-corrs = table('Size', [1, 5], 'VariableNames', names, 'VariableTypes', types);
+corrs = table('Size', [size(dataIn, 1), 4], 'VariableNames', names, 'VariableTypes', types);
 
 rSqrs = zeros([size(dataIn, 1) 1]);
 for i = 22:size(dataIn, 1) + 21
     try
         tempModel = findResultCorrelation(dataIn, i, event);
         
-        tempR = tempModel.Rsquared.Adjusted;
+        corrs.rSquAdj(i) = tempModel.Rsquared.Adjusted;
+        corrs.rSqu(i) = tempModel.Rsquared.Standard;
         
-        i
-        
-        rSqrs(i) = tempR;
+        corrs.compName(i) = dataIn{i - 22, 2}
     catch
         rSqrs(i) = NaN;
     end
